4dfe9e5171c60f7854bf374dad839785a9ae2215
NickDiPreta <nick@getperch.app>
2023-02-22 12:51:26 -0500

    added server and route for create account

---
+++ api/server.go
@@ -0,0 +1,33 @@
+package api
+
+import (
+	db "github.com/NickDiPreta/simplebank/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+// Server serves HTTP requests for simplebank
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+// NewServer creates a new HTTP server and router
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	//routes
+	router.POST("/accounts", server.createAccount)
+
+	server.router = router
+	return server
+}
+
+// Start runs the http server at an address
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) map[string]interface{} {
+	return gin.H{"error": err.Error()}
+}
